dbebdc74ee806eef8d6a5815b4557ea260b14171
ANKIREDDY2 <asee.cse2024@rmd.ac.in>
2026-06-11 18:49:29 +0530

    Create Largest subarray of 0's and 1's.java

---
+++ Largest subarray of 0's and 1's.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxLen(int[] arr) {
+        // Your code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum =0;
+        int maxlength=0;
+        map.put(0,-1);
+        for(int i=0 ;i<arr.length;i++)
+        {
+            if(arr[i]==0)
+            {
+                sum+=1;
+            }else{
+                sum+=-1;
+            }
+            if(map.containsKey(sum))
+            {
+                maxlength=Math.max(maxlength,i-(map.get(sum)));
+            }else{
+                map.put(sum,i);
+            }
+        }
+        return maxlength;
+    }
+}
